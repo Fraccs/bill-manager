e333cc21f265a5137fafea19e20c78288f9b174a
Fraccs <francyaltrebbia@gmail.com>
2021-11-27 08:18:06 +0100

    init

---
+++ Res/command_interpreter.cpp
@@ -0,0 +1,13 @@
+/*============================================================================
+ * Name        : command_interpreter.cpp
+ * Version     : Alpha
+ * Since       : 2021
+ * Author      : Aliprandi Francesco <aliprandifrancescopp@gmail.com>
+ * Web         : https://github.com/Fraccs/bill-manager
+ * Copyright   : N/D
+ * License     : N/D
+ * Last change : 27/11/2021
+ * Description : Source file containing command_interpreter.h functions definitions
+ *============================================================================*/
+
+#include "command_interpreter.h"
---
+++ Res/command_interpreter.h
@@ -0,0 +1,18 @@
+/*============================================================================
+ * Name        : command_interpreter.h
+ * Version     : Alpha
+ * Since       : 2021
+ * Author      : Aliprandi Francesco <aliprandifrancescopp@gmail.com>
+ * Web         : https://github.com/Fraccs/bill-manager
+ * Copyright   : N/D
+ * License     : N/D
+ * Last change : 27/11/2021
+ * Description : Header containing commandline interface functions
+ *============================================================================*/
+
+#ifndef _COMMAND_INTERPRETER_H
+#define _COMMAND_INTERPRETER_H
+
+
+
+#endif
