1f7bced43f45f73e72d6e09d5ee86a33e020d3e1
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2021-11-29 19:37:48 +0100

    interface_init

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@
  * Web         : https://github.com/Fraccs/bill-manager
  * Copyright   : N/D
  * License     : N/D
- * Last change : 25/11/2021
+ * Last change : 29/11/2021
  * Description : Main 
  *============================================================================*/
 
 #include <iostream>
+#include <vector>
 
 #include "Res/client.h"
 #include "Res/logs.h"
@@ -18,17 +19,40 @@
 
 int main() {
     Client client;
-    std::string username;
-    std::string password;
-    bool done = false;
-    int choice;
+    std::string command;
+    std::string argument;
+    std::vector<std::string> flags;
 
-    while(!done) {
+    while(true) {
         std::cout << ">: ";
+        std::cin >> command;
 
-        
+        if(command == "bill -q" || command == "bill --quit") return 0;
 
-        
+        flags = find_flags(command);
+
+        // Iterating through the flags
+        for(int i = 0; i < flags.size(); i++) {
+            if(flags[i] == "-a" || flags[i] == "--add") {
+                argument = get_argument(command, flags[i]);  
+            }
+
+            if(flags[i] == "-r" || flags[i] == "--register") {
+                argument = get_argument(command, flags[i]);
+            
+                // Get the password (no echo)
+            }
+
+            if(flags[i] == "--login") {
+                argument = get_argument(command, flags[i]);
+            
+                // Get the password (no echo)
+            }
+
+            if(flags[i] == "--logout") {
+                std::cout << "Logged out from user '" << client.getUsername() << "' successfully." << std::endl;
+            }
+        }
     }
 
     return 0;
