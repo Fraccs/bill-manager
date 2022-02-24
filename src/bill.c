911bb4533cf872faa52b84246a0a414c48ac0721
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2022-02-24 18:40:16 +0100

    Added get functions for struct bill members and fixed comments

--- src/bill.c
+++ src/bill.c
@@ -6,13 +6,14 @@
  * Web         : https://github.com/Fraccs/bill-manager
  * Copyright   : N/D
  * License     : N/D
- * Last change : 23/02/2022
+ * Last change : 24/02/2022
  * Description : Source file containing bill related structs and functions definitions
  *============================================================================*/
 
 #include "bill.h"
 
-// Struct bill
+/* Bill struct, note that this is an opaque type and only pointer declaration is valid.
+Use the predefined functions to access members*/
 typedef struct bs {
     char type[TYPE_MAXLEN + 1];
     char due_date[DDAT_MAXLEN + 1];
@@ -27,9 +28,9 @@ bill *billCreate() {
 
     if(b == NULL) return NULL;
 
-    memset(b->type, 0, TYPE_MAXLEN);
-    memset(b->due_date, 0, DDAT_MAXLEN);
-    memset(b->paid_date, 0, PDAT_MAXLEN);
+    memset(b->type, 0, TYPE_MAXLEN + 1);
+    memset(b->due_date, 0, DDAT_MAXLEN + 1);
+    memset(b->paid_date, 0, PDAT_MAXLEN + 1);
     b->paid = false;
     b->cost = 0;
 
@@ -91,4 +92,51 @@ int billSetCost(bill *b, float cost) {
     b->cost = cost;
 
     return 0;
+}
+
+/* Loads dest with the passed bill's type
+(dest_s is the size of dest excluding the additional NULL terminating character '\0')*/
+int billGetType(bill *b, char *dest, size_t dest_s) {
+    if(b == NULL || dest == NULL || dest_s < DDAT_MAXLEN) return -1;
+    
+    memset(dest, 0, dest_s + 1);
+    strncpy(dest, b->type, dest_s);
+
+    return 0;
+}
+
+/* Loads dest with the passed bill's due_date
+(dest_s is the size of dest excluding the additional NULL terminating character '\0')*/
+int billGetDueDate(bill *b, char *dest, size_t dest_s) {
+    if(b == NULL || dest == NULL || dest_s < DDAT_MAXLEN) return -1;
+
+    memset(dest, 0, dest_s + 1);
+    strncpy(dest, b->due_date, dest_s);
+
+    return 0;
+}
+
+/* Loads dest with the passed bill's paid_date
+(dest_s is the size of dest excluding the additional NULL terminating character '\0')*/
+int billGetPaidDate(bill *b, char *dest, size_t dest_s) {
+    if(b == NULL || dest == NULL || dest_s < PDAT_MAXLEN) return -1;
+
+    memset(dest, 0, dest_s + 1);
+    strncpy(dest, b->paid_date, dest_s);
+
+    return 0;
+}
+
+// Returns if the passed bill was paid
+int billGetPaid(bill *b) {
+    if(b == NULL) return -1;
+    
+    return b->paid; 
+}
+
+// Returns the passed-bill's cost
+float billGetCost(bill *b) {
+    if(b == NULL) return -1;
+
+    return b->cost;
 }
--- src/bill.h
+++ src/bill.h
@@ -6,7 +6,7 @@
  * Web         : https://github.com/Fraccs/bill-manager
  * Copyright   : N/D
  * License     : N/D
- * Last change : 23/02/2022
+ * Last change : 24/02/2022
  * Description : Header file containing bill related structs and functions declarations
  *============================================================================*/
 
@@ -21,7 +21,8 @@
 
 #define TYPE_MAXLEN 20
 
-// Struct bill
+/* Bill struct, note that this is an opaque type and only pointer declaration is valid.
+Use the predefined functions to access members*/
 typedef struct bs bill;
 
 // Returns a pointer to the memory allocated on the heap for a new bill
@@ -41,5 +42,23 @@ int billSetPaid(bill *b, bool paid);
 
 // Set the cost of a bill
 int billSetCost(bill *b, float cost);
+
+/* Loads dest with the passed bill's type
+(dest_s is the size of dest excluding the additional NULL terminating character '\0')*/
+int billGetType(bill *b, char *dest, size_t dest_s);
+
+/* Loads dest with the passed bill's due_date
+(dest_s is the size of dest excluding the additional NULL terminating character '\0')*/
+int billGetDueDate(bill *b, char *dest, size_t dest_s);
+
+/* Loads dest with the passed bill's paid_date
+(dest_s is the size of dest excluding the additional NULL terminating character '\0')*/
+int billGetPaidDate(bill *b, char *dest, size_t dest_s);
+
+// Returns if the passed bill was paid
+int billGetPaid(bill *b);
+
+// Returns the passed-bill's cost
+float billGetCost(bill *b); 
 
 #endif
