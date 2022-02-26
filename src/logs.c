d3fd4a4d4fd1798fc3d8f70008a21b3cd628ea97
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2022-02-26 22:09:29 +0100

    Adapted date.c to new date module functions names

--- src/logs.c
+++ src/logs.c
@@ -6,7 +6,7 @@
  * Web         : https://github.com/Fraccs/bill-manager
  * Copyright   : N/D
  * License     : N/D
- * Last change : 23/02/2022
+ * Last change : 26/02/2022
  * Description : Source file containing logs related functions definition
  *============================================================================*/
 
@@ -20,7 +20,7 @@ int logEvent(const char *logfile_name, const char *str) {
 
     if(log_file == NULL) return -1; 
 
-    machineTimeSecAccurate(time_string, 20);
+    dateSecAccurate(time_string, 19);
 
     // Appending to the last istance of the file 
     fprintf(log_file, "%s | %s\n", time_string, str);
