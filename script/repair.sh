cb2b3ac1b9764dce37fef60e9c6a09e6f2215313
Aliprandi Francesco <aliprandifrancescopp@gmail.com>
2022-05-05 08:07:59 -0700

    [v1.2.0]: fixed 'command not found' bug

--- script/repair.sh
+++ script/repair.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 # Directories and files
-PATH="/usr/local/bin/"
+BINPATH="/usr/local/bin/"
 BIN="billman"
 STORAGE="/var/lib/billman/"
 GROUP="billman"
-
-# Command PATHS
-SUDO="/usr/bin/sudo"
-GROUPADD="/usr/sbin/groupadd"
-CHMOD="/usr/bin/chmod"
-GREP="/usr/bin/grep"
-GROUPS="/usr/bin/groups"
-USERMOD="/usr/sbin/usermod"
-ID="/usr/bin/id"
-MKDIR="/usr/bin/mkdir"
-LS="/usr/bin/ls"
-CHGRP="/usr/bin/chgrp"
 
 # Colors
 GREEN='\033[1;32m'
@@ -32,45 +20,45 @@ CRITICAL="${RED}Critical [X]"
 ABORT="${RED}Aborting."
 
 # Checking if the binary is in PATH
-if test -f "$PATH$BIN"; then
-    echo "$PASSED: '$BIN' is in PATH: '$PATH'${NC}"
+if test -f "$BINPATH$BIN"; then
+    echo "$PASSED: '$BIN' is in PATH: '$BINPATH'${NC}"
 else
-    echo "$CRITICAL: '$BIN' is not in PATH: '$PATH'${NC}"
+    echo "$CRITICAL: '$BIN' is not in PATH: '$BINPATH'${NC}"
     echo "${YELLOW}Please run 'install.sh' with root permissions and repeat the installation${NC}"
     echo $ABORT
     exit
 fi
 
 # Checking for the executable bit
-if test -x $PATH$BIN; then
+if test -x $BINPATH$BIN; then
     echo "$PASSED: '$BIN' is executable by '$USER'${NC}"
 else
     echo "$ERROR: '$BIN' is not executable by '$USER'${NC}"
     echo "$FIXING: Adding the executable bit to '$BIN'${NC}"
     
-    $SUDO $CHMOD "+x" $PATH$BIN # chmod +x $PATH$BIN
+    sudo chmod +x $BINPATH$BIN
     echo "$FIXED: Added the executable bit to '$BIN'${NC}"
 fi
 
 # Checking if group 'billman' exists
-if $GREP -q $GROUP /etc/group; then
+if grep -q $GROUP /etc/group; then
     echo "$PASSED: the group '$GROUP' exists${NC}"
 else
     echo "$ERROR: the group '$GROUP' does not exist${NC}"
     echo "$FIXING: Creating the group '$GROUP'${NC}" 
 
-    $SUDO $GROUPADD $GROUP # groupadd billman
+    sudo groupadd $GROUP
     echo "$FIXED: Created the group '$GROUP'${NC}"
 fi
 
 # Checking if user is in the 'billman' group
-if $ID -nG "$USER" | $GREP -qw "$GROUP"; then
+if id -nG "$USER" | grep -qw "$GROUP"; then
     echo "$PASSED: '$USER' is in the group '$GROUP'${NC}"
 else
     echo "$ERROR: '$USER' is not in the group '$GROUP'${NC}"
     echo "$FIXING: Adding '$USER' to the group '$GROUP'${NC}" 
 
-    $SUDO $USERMOD -aG $GROUP $USER # usermod -aG billman $USER
+    sudo usermod -aG $GROUP $USER
     echo "$FIXED: Created the group '$GROUP'${NC}"
 fi
 
@@ -81,18 +69,18 @@ else
     echo "$ERROR: the storage directory '$STORAGE' does not exist${NC}"
     echo "$FIXING: Creating the storage directory '$STORAGE'${NC}" 
 
-    $SUDO $MKDIR $STORAGE # mkdir /var/lib/billman
+    sudo mkdir $STORAGE
     echo "$FIXED: Created the storage directory '$STORAGE'${NC}"
 fi
 
 # Checking if the 'billman' group is the owner of the storage directory
-if $LS "/var/lib" -l | $GREP -qw "$GROUP"; then
+if ls "/var/lib" -l | grep -qw "$GROUP"; then
     echo "$PASSED: the group '$GROUP' is the owner of the storage directory '$STORAGE'${NC}"
 else
     echo "$ERROR: the group '$GROUP' is not the owner of the storage directory '$STORAGE'${NC}"
     echo "$FIXING: Changing the owner group of the storage directory '$STORAGE'${NC}" 
 
-    $SUDO $CHGRP $GROUP $STORAGE # chgrp billman /var/lib/billman
+    sudo chgrp $GROUP $STORAGE
     echo "$FIXED: Changed the owner group of storage directory '$STORAGE'${NC}"
 fi
 
@@ -103,6 +91,6 @@ else
     echo "$ERROR: the group '$GROUP' does not have permissions over the storage directory '$STORAGE'${NC}"
     echo "$FIXING: Adding permissions to the group '$GROUP' over the storage directory '$STORAGE'${NC}" 
 
-    $SUDO $CHMOD g+rwx $STORAGE # chmod g+rwx /var/lib/billman
+    sudo chmod g+rwx $STORAGE # chmod g+rwx /var/lib/billman
     echo "$FIXED: Added permissions to the group '$GROUP' over the storage directory '$STORAGE'${NC}"
 fi
